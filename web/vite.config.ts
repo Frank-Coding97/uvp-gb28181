3e38a1d8773ad1e72144a46cac26936c10f7ab94
menglulu <252657030@qq.com>
2026-07-19 09:29:45 +0800

    chore(web): disable dev server auto-open

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
         base: env.VITE_PUBLIC_PATH,
         server: {
             // host: "0.0.0.0",
-            open: true,
+            open: false,
             port: 5177,
             // 为开发服务器配置自定义代理规则-用于开发时的代理
             proxy: {
