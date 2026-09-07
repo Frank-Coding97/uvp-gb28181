85dc360eba29710cb946d914fbc87a87acd5a2a6
menglulu <252657030@qq.com>
2026-09-07 14:23:53 +0800

    docs(windows): collect Redis bundled dependency notices

--- deploy/windows/redis/collect-sources.sh
+++ deploy/windows/redis/collect-sources.sh
@@ -6,6 +6,13 @@ mkdir -p redistribution-sources licenses
 cp redis-335554f1.tar.gz redistribution-sources/
 cp -r /usr/src/cygwin-3.6.10-1.src /usr/src/openssl-3.5.8-1.src /usr/src/zlib-1.3.2-1.src redistribution-sources/
 cp source/COPYING licenses/REDIS-COPYING
+cp source/deps/hiredis/COPYING licenses/HIREDIS-COPYING
+cp source/deps/lua/COPYRIGHT licenses/LUA-COPYRIGHT
+cp source/deps/hdr_histogram/LICENSE.txt licenses/HDR-HISTOGRAM-LICENSE
+cp source/deps/hdr_histogram/COPYING.txt licenses/HDR-HISTOGRAM-COPYING
+cp source/deps/fpconv/LICENSE.txt licenses/FPCONV-LICENSE
+# Linenoise carries its redistribution notice in the source file header.
+cp source/deps/linenoise/linenoise.c licenses/LINENOISE-SOURCE-NOTICE.c
 cp /usr/share/doc/openssl/LICENSE.txt licenses/OPENSSL-LICENSE
 for name in COPYING COPYING.LIB COPYING.LIBGLOSS COPYING.NEWLIB COPYING3 COPYING3.LIB; do
   tar -xOf /usr/src/cygwin-3.6.10-1.src/newlib-cygwin-3.6.10.tar.bz2 "newlib-cygwin/$name" > "licenses/CYGWIN-$name"
